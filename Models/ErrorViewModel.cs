470ae7e931cebc7ea15df629a3fb8ec9fef6c831
Alejandro Botero <a.b.hincapie@gamail.com>
2024-11-19 21:02:38 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesAppCrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
